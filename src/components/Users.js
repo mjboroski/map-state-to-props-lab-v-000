fdf302c98a26a38bce49c0cdbf0efce942f2845e
Michael Boroski <mjboroski@gmail.com>
2018-08-04 04:03:09 +0000

    automatically backed up by learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Users extends Component {
+export class Users extends Component {
 
   render() {
     let users = this.props.users.map((user, index) => <li key={index}>{user.userName}</li>);
@@ -23,4 +23,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Users)
+export const ConnectedUsers = connect(mapStateToProps)(Users)
