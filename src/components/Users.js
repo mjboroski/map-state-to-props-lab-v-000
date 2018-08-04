cf7c04ae1fa061ac094ef95599298674d41bdf95
Michael Boroski <mjboroski@gmail.com>
2018-08-04 04:12:26 +0000

    automatically backed up by learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export class Users extends Component {
+class Users extends Component {
 
   render() {
     let users = this.props.users.map((user, index) => <li key={index}>{user.userName}</li>);
