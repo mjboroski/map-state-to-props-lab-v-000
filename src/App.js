7aec1cd1c5b218b8c1551e6990ecc62f78f4b9d4
Michael Boroski <mjboroski@gmail.com>
2018-08-04 04:02:53 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput'
-import Users from './components/Users'
+import ConnectedUsers from './components/Users'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <UserInput store={this.props.store}/>
-        <Users />
+        <ConnectedUsers />
       </div>
     );
   }
