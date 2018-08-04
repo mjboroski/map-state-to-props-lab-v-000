b7b3699cf36904fce3073a4689c56440a68cc82e
Michael Boroski <mjboroski@gmail.com>
2018-08-04 03:48:41 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import manageUsers from './reducers/manageUsers'
 
+export const configureStore = () => {
+  return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+}
 
-// add imports and code
-
+let store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
